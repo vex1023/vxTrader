eec0028c74d562d88a86cf4063c5bc5e8771464e
vex1023 <vex1023@qq.com>
2016-09-28 14:25:30 +0800

    增加test_gfTrader.py

---
+++ tests/test_gftrader.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class gfTraderTestCase(unittest.TestCase):
+    def test_gfLoginSession(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
