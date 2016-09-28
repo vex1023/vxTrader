73a0d0ab0a755ec086b17871baf03f2f6ac5a13e
李宝 <libao@vex1023.com>
2016-09-29 00:30:28 +0800

    增加了测试用例

--- tests/test_gftrader.py
+++ tests/test_gftrader.py
@@ -5,17 +5,19 @@ import os
 
 class gfTraderTestCase(unittest.TestCase):
     def setUp(self):
+        brokerid = os.getenv('brokerid', '')
         account = os.getenv('account', '')
         password = os.getenv('password', '')
         kwargs = os.getenv('kwargs', '')
 
-        if account == '' or password == '':
+        if brokerid == '' or account == '' or password == '':
             raise EnvironmentError('enviroment value account(%s) or password(%s) is not set' % (account, password))
 
-        self.trader = TraderFactory.create('gf', account, password)
+        self.trader = TraderFactory.create(brokerid, account, password)
 
     def test_gfLoginSession(self):
         print(self.trader.portfolio)
+        print(self.trader.orderlist)
 
 
 if __name__ == '__main__':
