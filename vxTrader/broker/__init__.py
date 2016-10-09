f8a6c8531aa04e92a8711f5ce415eecefafc6459
李宝 <libao@vex1023.com>
2016-10-09 23:33:50 +0800

    增加broker 的__init__.py内可引入的包

--- vxTrader/broker/__init__.py
+++ vxTrader/broker/__init__.py
@@ -2,8 +2,9 @@
 '''
 broker基础类
 '''
+from .WebTrader import WebTrader, LoginSession
 from .gfTrader import gfTrader
 from .xqTrader import xqTrader
 from .yjbTrader import yjbTrader
 
-__all__ = ['gfTrader', 'yjbTrader', 'xqTrader']
+__all__ = ['gfTrader', 'yjbTrader', 'xqTrader', 'WebTrader', 'LoginSession']
