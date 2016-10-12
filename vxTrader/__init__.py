0fbf8d821096c14934eba9b55fb1d054c79c88c8
李宝 <libao@vex1023.com>
2016-10-12 21:32:36 +0800

    增加批量创建traders函数：load_traders

--- vxTrader/__init__.py
+++ vxTrader/__init__.py
@@ -8,6 +8,7 @@ __email__ = 'vex1023@qq.com'
 
 __all__ = ['logger', 'TraderFactory']
 
+import configparser
 import logging
 
 from vxTrader.PrettyLogger import add_console_logger
@@ -37,7 +38,15 @@ class TraderFactory():
         return cls
 
 
+# 自动加载相应的broker
+from vxTrader.broker import *
+
+
+
 def create_trader(brokerID, account, password, **kwargs):
+    '''
+    创建单个trader的接口
+    '''
     brokerID = brokerID.lower()
     instance = TraderFactory.instance.get(brokerID, None)
 
@@ -47,5 +56,32 @@ def create_trader(brokerID, account, password, **kwargs):
         raise NotImplementedError(err_msg)
 
     return instance(account, password, **kwargs)
-# 自动加载相应的broker
-from vxTrader.broker import *
+
+
+# 根据配置文件来创建
+def load_traders(ConfigFile='/etc/vxQuant/vxTrader.conf'):
+    '''
+    通过配置文件来批量创建traders
+    :param ConfigFile:
+    :return: traders的dict
+    '''
+    config = configparser.ConfigParser()
+    config.read(ConfigFile)
+
+    traders = {}
+
+    sections = config.sections()
+    for section in sections:
+        # 读取全部section下的所有items
+        kwargs = dict(config.items(section))
+
+        brokerid = kwargs.pop('brokerid', '')
+        account = kwargs.pop('account', '')
+        password = kwargs.pop('password', '')
+        # 检查配置是否正确
+        if brokerid == '' or account == '' or password == '':
+            raise ValueError('brokerid: %s, account: %s, password: %s' % (brokerid, account, password))
+
+        traders[section] = create_trader(brokerid, account, password, **kwargs)
+
+    return traders
